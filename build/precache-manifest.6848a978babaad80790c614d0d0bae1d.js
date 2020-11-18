097f739675fc581c4f01a9e501e475863ded483b
Brent Bagapuro <brentbagapuro@gmail.com>
2020-11-18 11:17:05 +0800

    added build

---
+++ build/precache-manifest.6848a978babaad80790c614d0d0bae1d.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ffed5334b0737d659badba0c403724b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9dd396da96d51762c86",
+    "url": "/static/css/main.cb46a0d5.chunk.css"
+  },
+  {
+    "revision": "480972f00a2d4235b158",
+    "url": "/static/js/2.e1e0fe09.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.e1e0fe09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9dd396da96d51762c86",
+    "url": "/static/js/main.e4eab400.chunk.js"
+  },
+  {
+    "revision": "f27c83d3936273ec94c2",
+    "url": "/static/js/runtime-main.119fcb5b.js"
+  },
+  {
+    "revision": "f30ae4e2b4de1aba1f25db3533137c89",
+    "url": "/static/media/311-Womens-Shaping-Skinny-Jeans-1.f30ae4e2.jpg"
+  },
+  {
+    "revision": "58fb07df6206233c8846304e8b9dfd8d",
+    "url": "/static/media/311-Womens-Shaping-Skinny-Jeans-2.58fb07df.jpg"
+  },
+  {
+    "revision": "778fbaded8cbe2c829d652873b9e3adb",
+    "url": "/static/media/311-Womens-Shaping-Skinny-Jeans-3.778fbade.jpg"
+  },
+  {
+    "revision": "3ffbb247438f6bf8501c9638978cfe67",
+    "url": "/static/media/Autograph-Flag-Slim-Fit-Polo-1.3ffbb247.jpg"
+  },
+  {
+    "revision": "11305a19431f3c6b049f03c0b8ecbda1",
+    "url": "/static/media/Autograph-Flag-Slim-Fit-Polo-2.11305a19.jpg"
+  },
+  {
+    "revision": "e67d3f38a8c3e7705bc347ecb91f8467",
+    "url": "/static/media/Autograph-Flag-Slim-Fit-Polo-3.e67d3f38.jpg"
+  },
+  {
+    "revision": "58ca2e1fa62ae902b60548bed4e0c0b6",
+    "url": "/static/media/Core-Tech-Logo-T-Shirt-1.58ca2e1f.jpg"
+  },
+  {
+    "revision": "0aa152b05c60633740308fb5343a3b88",
+    "url": "/static/media/Core-Tech-Logo-T-Shirt-2.0aa152b0.jpg"
+  },
+  {
+    "revision": "6287803ff22a333995d70e85444ea4a4",
+    "url": "/static/media/Core-Tech-Logo-T-Shirt-3.6287803f.jpg"
+  },
+  {
+    "revision": "a6d9cc54f8be00ba3b7bc2087ab0da55",
+    "url": "/static/media/Essential-3-Stripes-Wind-Pants-1.a6d9cc54.jpg"
+  },
+  {
+    "revision": "e514f23abff4212ddd201d01f966ea96",
+    "url": "/static/media/Essential-3-Stripes-Wind-Pants-2.e514f23a.jpg"
+  },
+  {
+    "revision": "aaa71ae0cb281b7a3110d01fdf0699af",
+    "url": "/static/media/Essential-3-Stripes-Wind-Pants-3.aaa71ae0.jpg"
+  },
+  {
+    "revision": "f1df5d975da7c70e70bf30d599d2e32f",
+    "url": "/static/media/Featherlight-Cap-1.f1df5d97.jpg"
+  },
+  {
+    "revision": "6591d8da8efb852fe3c59f489c3535e5",
+    "url": "/static/media/Featherlight-Cap-2.6591d8da.jpg"
+  },
+  {
+    "revision": "802e484d4865e5dd3e0419ff6b7c3667",
+    "url": "/static/media/Graphics-Linear-Logo-Short-Sleeves-Tee-1.802e484d.jpg"
+  },
+  {
+    "revision": "130de767d1ebfb636a417d3158bfbf47",
+    "url": "/static/media/Graphics-Linear-Logo-Short-Sleeves-Tee-2.130de767.jpg"
+  },
+  {
+    "revision": "279e2269befeba2d9d91ee64351e7e71",
+    "url": "/static/media/Graphics-Linear-Logo-Short-Sleeves-Tee-3.279e2269.jpg"
+  },
+  {
+    "revision": "1eb0647c8383a3b920c4d00b8104d863",
+    "url": "/static/media/Ivory-Pussybow-Neck-34-Sleeve-Top-1.1eb0647c.jpg"
+  },
+  {
+    "revision": "a6381c171f2b6982e008211394c7290a",
+    "url": "/static/media/Ivory-Pussybow-Neck-34-Sleeve-Top-2.a6381c17.jpg"
+  },
+  {
+    "revision": "0409b416f779ba29fa9f960467809490",
+    "url": "/static/media/Ivory-Pussybow-Neck-34-Sleeve-Top-3.0409b416.jpg"
+  },
+  {
+    "revision": "d3baa17b49a55272d7aa9a8c9fb878f0",
+    "url": "/static/media/Mens-Sportswear-Icon-Futura-Tee-1.d3baa17b.jpg"
+  },
+  {
+    "revision": "0681edbba5fbc2fa71cd23654d71f6da",
+    "url": "/static/media/Mens-Sportswear-Icon-Futura-Tee-2.0681edbb.jpg"
+  },
+  {
+    "revision": "ddc1c6354b0a93d5d4898ce2e0e70374",
+    "url": "/static/media/Mens-Sportswear-Icon-Futura-Tee-3.ddc1c635.jpg"
+  },
+  {
+    "revision": "d625c3811969a1ae6232098431f3d30e",
+    "url": "/static/media/Mens.d625c381.jpg"
+  },
+  {
+    "revision": "3aae2a3f02ea3d1ac97c1b07585b48ae",
+    "url": "/static/media/Original-Logo-Crew-Neck-Short-Sleeve-Tee-1.3aae2a3f.jpg"
+  },
+  {
+    "revision": "fce079e38a7ab9e5a33f135345746dc1",
+    "url": "/static/media/Original-Logo-Crew-Neck-Short-Sleeve-Tee-2.fce079e3.jpg"
+  },
+  {
+    "revision": "fa7cf76c1f3dd367051872dfc8bb7341",
+    "url": "/static/media/Original-Logo-Crew-Neck-Short-Sleeve-Tee-3.fa7cf76c.jpg"
+  },
+  {
+    "revision": "abc35102900711be231dd873f5c63fbb",
+    "url": "/static/media/Stan-Smith-Shoes-2.abc35102.jpg"
+  },
+  {
+    "revision": "eae45b23ba8eb71075e01b68e247f9c4",
+    "url": "/static/media/Stan-Smith-Shoes-3.eae45b23.jpg"
+  },
+  {
+    "revision": "947bb7108a3becd23b3c2cb3f03a1242",
+    "url": "/static/media/Stan-Smith-Shoes-4.947bb710.jpg"
+  },
+  {
+    "revision": "a3170d26e14fbe43e7b0ef5157053960",
+    "url": "/static/media/Stan-Smith-Shoes-5.a3170d26.jpg"
+  },
+  {
+    "revision": "a7e6d00bde5ecd0404c6d18ef33e7fbc",
+    "url": "/static/media/Textured-Printed-Dress-1.a7e6d00b.jpg"
+  },
+  {
+    "revision": "fe921f9c7f28d15a550aad1e7993b563",
+    "url": "/static/media/Textured-Printed-Dress-2.fe921f9c.jpg"
+  },
+  {
+    "revision": "953c3cf9a02f363ab40ff2d60418d547",
+    "url": "/static/media/Textured-Printed-Dress-3.953c3cf9.jpg"
+  },
+  {
+    "revision": "89ce6ec79414db96a5b0edd1116bbb8b",
+    "url": "/static/media/Womens.89ce6ec7.jpg"
+  }
+]);
